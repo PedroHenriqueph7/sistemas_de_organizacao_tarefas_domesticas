aac97fb45255e09823d64a82dcedf39dd606d266
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2025-07-21 08:05:04 -0300

    Inserção dos dados das entidades Pessoa, Tarefa e CategoriaTarefa.

--- src/main/java/org/projeto/models/CategoriaTarefa.java
+++ src/main/java/org/projeto/models/CategoriaTarefa.java
@@ -1,5 +1,64 @@
 package org.projeto.models;
 
 public class CategoriaTarefa {
-    
+    private Integer id;
+    private String category_name;
+
+    public CategoriaTarefa(){}
+
+    public CategoriaTarefa(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((category_name == null) ? 0 : category_name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CategoriaTarefa other = (CategoriaTarefa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (category_name == null) {
+            if (other.category_name != null)
+                return false;
+        } else if (!category_name.equals(other.category_name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaTarefa [id=" + id + ", category_name=" + category_name + "]";
+    }
 }
--- src/main/java/org/projeto/models/Tarefa.java
+++ src/main/java/org/projeto/models/Tarefa.java
@@ -1,5 +1,108 @@
 package org.projeto.models;
 
+import org.projeto.models.enums.PrioridadeTarefa;
+import org.projeto.models.enums.StatusTarefa;
+
 public class Tarefa {
-    
+    private Integer id;
+    private String task_name;
+    private PrioridadeTarefa priority;
+    private StatusTarefa status;
+    private Pessoa person;
+    private CategoriaTarefa category;
+
+    public Tarefa(){}
+
+    public Tarefa(String task_name, PrioridadeTarefa priority, StatusTarefa status, Pessoa person, CategoriaTarefa category) {
+        this.task_name = task_name;
+        this.priority = priority;
+        this.status = status;
+        this.person = person;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public void setTask_name(String task_name) {
+        this.task_name = task_name;
+    }
+
+    public PrioridadeTarefa getPriority() {
+        return priority;
+    }
+
+    public void setPriority(PrioridadeTarefa priority) {
+        this.priority = priority;
+    }
+
+    public StatusTarefa getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTarefa status) {
+        this.status = status;
+    }
+
+    public Pessoa getPerson() {
+        return person;
+    }
+
+    public void setPerson(Pessoa person) {
+        this.person = person;
+    }
+
+    public CategoriaTarefa getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoriaTarefa category) {
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((task_name == null) ? 0 : task_name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tarefa other = (Tarefa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (task_name == null) {
+            if (other.task_name != null)
+                return false;
+        } else if (!task_name.equals(other.task_name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa [id=" + id + ", task_name=" + task_name + ", priority=" + priority + ", status=" + status
+                + ", person=" + person + ", category=" + category + "]";
+    }
 }
